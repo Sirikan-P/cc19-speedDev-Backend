f11f1f5ef5120f956a4d653fecf091c3489726a1
Sirikan-P <sirikan.interior@gmail.com>
2025-03-10 16:04:01 +0700

    change payment-route

--- routes/payment-route.js
+++ routes/payment-route.js
@@ -1,1 +1,18 @@
-console.log('test-payment')
+//import lib..
+const express = require("express")
+const router = express.Router()
+
+//import contoller..
+
+
+//import middleware
+
+
+//route
+//@ENDPOINT http://localhost:8000/
+// router.get()
+// router.patch()
+// router.delete()
+
+//export
+module.exports = router
